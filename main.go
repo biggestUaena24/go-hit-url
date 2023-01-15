89e13af9e2d784daef4ae8b6e022a8a37872f787
Geun Han Chung <a2782541671@gmail.com>
2023-01-15 00:40:38 -0500

    Job scraping done

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
 
 	"github.com/PuerkitoBio/goquery"
@@ -18,16 +20,42 @@ var baseURL = "https://www.saramin.co.kr/zf_user/search/recruit?&searchword=pyth
 
 
 func main() {
+	var jobs []extractedJob
+	c := make(chan []extractedJob)
 	totalPages := getPages()
 
 	for i:=0; i<totalPages; i++{
-		getPage(i)
+		go getPage(i, c)
+	}
+
+	for i:=0; i<totalPages;i++{
+		jobs = append(jobs, <-c...)
+	}
+
+	writeJobs(jobs)
+}
+
+func writeJobs(jobs []extractedJob) {
+	file, err := os.Create("jobs.csv")
+	checkErr(err)
+
+	w := csv.NewWriter(file)
+
+	defer w.Flush()
+	headers := []string{"job link", "title", "location"}
+	checkErr(w.Write(headers))
+
+	for _, job := range(jobs){
+		info := []string{"https://www.saramin.co.kr/zf_user/jobs/relay/view?rec_idx=" + job.id, " " + job.title, job.location}
+		checkErr(w.Write(info))
 	}
 }
 
-func getPage(page int) []extractedJob{
+func getPage(page int, mainChannel chan[]extractedJob){
 	var jobResults []extractedJob
+	c := make(chan extractedJob)
 	pageURL := baseURL + "&recruitPage="+ strconv.Itoa(page + 1)
+	fmt.Println("Extracting from URL: ", pageURL)
 	
 	res, err := http.Get(pageURL)
 	checkErr(err)
@@ -40,18 +68,25 @@ func getPage(page int) []extractedJob{
 	jobCards := doc.Find(".item_recruit")
 
 	jobCards.Each(func (i int, s *goquery.Selection){
-		id,_ := s.Attr("value")
-		title, _ := s.Find(".area_job>.job_tit>a").Attr("title")
-		location := ""
-		location += s.Find(".area_job>.job_condition>span>a").Text()
-		
-		job := extractedJob{id: id, title: title, location:location}
-		jobResults = append(jobResults, job)
-		fmt.Println(job.id, job.title, job.location)
+		go extractJob(s, c)
 	})
 
-	return jobResults
+	for i:=0; i < jobCards.Length();i++{
+		jobResults = append(jobResults, <-c)
+	}
+
+	mainChannel <- jobResults
+}
+
+func extractJob (s *goquery.Selection, c chan extractedJob){
+
+	id,_ :=  s.Attr("value")
+	title, _ := s.Find(".area_job>.job_tit>a").Attr("title")
+	location := ""
+	location += s.Find(".area_job>.job_condition>span>a").Text()
 	
+	c <- extractedJob{id: id, title: title, location:location}
+
 
 }
 
